e53bec83ee383ed83908238eeb6fcf885fb3539e
RuiYuriAfricano <yuriafricano03@gmail.com>
2025-05-04 17:34:05 +0100

    feat: adjust-main

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  // Definir porta do servidor via variável de ambiente ou padrão 3000
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
 }
 bootstrap();
